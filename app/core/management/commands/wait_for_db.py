e5ab5495bb8cedf03a4748b7b4edb070e0f34116
Zunduo <zzd82073880@gmail.com>
2023-04-11 21:30:40 -0400

    configure the docker compose and add wait for db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -13,15 +13,15 @@ class Command(BaseCommand):
     """Django command to wait for database."""
 
     def handle(self, *args, **optios):
-        pass
-        # self.stdout.write('wating for database...')
-        # db_up = False
-        # while db_up is False:
-        #     try:
-        #         self.check(databases=['default'])
-        #         db_up = True
-        #     except (Psycopg2pError, OperationalError):
-        #         self.stdout.write('Database unavailable, wating 1 second ...')
-        #         time.sleep(1)
 
-        # self.stdout.write(self.style.SUCCESS('Database available!'))
+        self.stdout.write('wating for database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2pError, OperationalError):
+                self.stdout.write('Database unavailable, wating 1 second ...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
